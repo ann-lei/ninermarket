0ff12f019199824e5bab371529573c81818598c4
ann <alei1@charlotte.edu>
2025-04-05 15:48:32 -0400

    navbar done

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,15 @@
-function toggleNav() {
-    const nav = document.getElementById('navMenu');
-    nav.classList.toggle('active');
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("navMenu");
+
+    hamburger.addEventListener("click", function () {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+});
+  
+
+document.querySelector('.logo').addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  
